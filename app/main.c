7391cd9c535208b0dcdff384f6b6a540cdd4e6ba
Abhishek-Karmakar <abhishek.karmakar10@gmail.com>
2017-09-04 19:52:13 +0530

    Skeleton of the overall program

--- app/main.c
+++ app/main.c
@@ -14,12 +14,26 @@
 //! \addtogroup main
 //! @{
 //*****************************************************************************
+// include any header file.
+
+// create function to wait for a specified amount of time.
 
 int main(void)
 {
+	// Set any control registers for PortC pin 6
+	// Moder
+	//OTyper
+	//OSpeedr
+	//PUPDr
 
     while(1)
     {
+    	// Turn on the LED
+    	////BSRR r
+    	// Wait
+    	// Turn off the LED
+    	//BRR
+    	// Wait
     }
 }
 //! @}
